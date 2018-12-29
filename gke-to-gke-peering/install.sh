d77e7982db51ca1add598cb2bdd9c8162f0cf17b
Bob Lee <boblee@google.com>
2018-12-28 23:49:14 -0500

    Updated install check and readme with a backend service requirement of 4

--- gke-to-gke-peering/install.sh
+++ gke-to-gke-peering/install.sh
@@ -52,7 +52,7 @@ if ! meets_quota "${PROJECT_ID}" "IN_USE_ADDRESSES" 20; then
 fi
 
 ### Ensure that the Backend services quota is met
-if ! meets_quota "${PROJECT_ID}" "BACKEND_SERVICES" 8; then
+if ! meets_quota "${PROJECT_ID}" "BACKEND_SERVICES" 4; then
   echo "Refer to https://cloud.google.com/compute/quotas"
   echo "Terminating..."
   exit 1
