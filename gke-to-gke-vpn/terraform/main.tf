a6c47a7c1222ba2964d7ec6029a2a5c30111b390
balaguduru <bguduru@google.com>
2019-07-18 12:48:56 -0700

    Updating oauth scope for GKE node pool

--- gke-to-gke-vpn/terraform/main.tf
+++ gke-to-gke-vpn/terraform/main.tf
@@ -50,10 +50,12 @@ resource "google_container_node_pool" "cluster1_nodes" {
 
   node_config {
     oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
       "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/trace.append",
     ]
 
     image_type = "COS"
@@ -87,10 +89,12 @@ resource "google_container_node_pool" "cluster2_nodes" {
 
   node_config {
     oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
       "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/trace.append",
     ]
 
     image_type = "COS"
@@ -124,10 +128,12 @@ resource "google_container_node_pool" "cluster3_nodes" {
 
   node_config {
     oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
       "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/trace.append",
     ]
 
     image_type = "COS"
@@ -161,10 +167,12 @@ resource "google_container_node_pool" "cluster4_nodes" {
 
   node_config {
     oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
       "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/trace.append",
     ]
 
     image_type = "COS"
